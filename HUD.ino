f847f7b67282b974ac78e5acaf9633f569895961
Carlos Duran <durancdr96@gmail.com>
2023-06-21 19:46:34 +0200

    Added method which is able to draw a basic hud with indications.

--- HUD.ino
+++ HUD.ino
@@ -55,25 +55,8 @@ void setup() {
     for(;;); // Don't proceed, loop forever
   }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  // display.display();
-  // delay(2000); // Pause for 2 seconds
-
   // Clear the buffer
   display.clearDisplay();
-
-  // Draw a single pixel in white
-  // display.drawPixel(10, 10, SSD1306_WHITE);
-
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
-  // display.display();
-  // delay(2000);
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
 
   testdrawline();      // Draw many lines
 
@@ -92,91 +75,112 @@ float degToRad(float degree) {
 }
 
 void testdrawline() {
-  int16_t i;
-
-  display.clearDisplay(); // Clear display buffer  
-
-  delay(2000); // Pause for 2 seconds
-
-  display.clearDisplay(); // Clear display buffer
-
-  double halfScreen = (display.height()-1) / 2;
-
-  float x = (display.width()-1) / 2 - 40;
-  float y = halfScreen;
-
-  float i_p = (display.width()-1) / 2 + 40;
-  float j_p = halfScreen;
   
-  display.drawLine( x, y , i_p, j_p, SSD1306_WHITE);
 
-  display.display();
+  delay(1000);
 
-  //delay(500);
-  //display.clearDisplay();
- 
-  //float point[2];
-  //float point2[2];
+  display.clearDisplay();
 
-  //rotatePoint(point, x, y, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(25));
-  //rotatePoint(point2, i_p, j_p, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(25));
-
-  //display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
-
-  //display.display();
+  for (int speedCounter = 80; speedCounter > 50; speedCounter = speedCounter - 10) {
 
   for (int thisPin = 0; thisPin < 45; thisPin++) {
-
-    delay(100);
-    display.clearDisplay();
-   
-    float point[2];
-    float point2[2];
-  
-    rotatePoint(point, x, y, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(thisPin));
-    rotatePoint(point2, i_p, j_p, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(thisPin));
-  
-    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
-  
-    display.display();
-
+    drawHud(thisPin);
   }
 
   for (int thisPin = 45; thisPin > -45; thisPin--) {
-
-    delay(100);
-    display.clearDisplay();
-   
-    float point[2];
-    float point2[2];
-  
-    rotatePoint(point, x, y, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(thisPin));
-    rotatePoint(point2, i_p, j_p, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(thisPin));
-  
-    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
-  
-    display.display();
-
+    drawHud(thisPin);
   }
 
   for (int thisPin = -45; thisPin < 0; thisPin++) {
-
-    delay(100);
-    display.clearDisplay();
-   
-    float point[2];
-    float point2[2];
+    drawHud(thisPin);
+  }
   
-    rotatePoint(point, x, y, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(thisPin));
-    rotatePoint(point2, i_p, j_p, ((display.width()-1) / 2), ((display.height()-1) / 2), degToRad(thisPin));
-  
-    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
-  
-    display.display();
-
   }
   
   delay(80000); // Pause for 2 seconds
+}
+
+void drawHud(float thisPin)
+{
+    display.clearDisplay();
+    
+    double halfScreen = (display.height()-1) / 2;
+
+    float x = (display.width()-1) / 2 - 40;
+    float y = halfScreen;
+
+    float i_p = (display.width()-1) / 2 + 40;
+    float j_p = halfScreen;
+
+    float centerX = ((display.width()-1) / 2);
+    float centerY = ((display.height()-1) / 2);
+
+    float point[2];
+    float point2[2];   
+  
+    rotatePoint(point, x, y, centerX, centerY, degToRad(thisPin));
+    rotatePoint(point2, i_p, j_p, centerX, centerY, degToRad(thisPin));
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(point2[0] - 5, point2[1] + 2);
+    display.cp437(true);  
+  
+    display.write("0");
+  
+    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
+    // -------------------------------------------------------------------
+    rotatePoint(point, x + 20, y + 10, centerX, centerY, degToRad(thisPin));
+    rotatePoint(point2, i_p - 20, j_p + 10, centerX, centerY, degToRad(thisPin));
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(point2[0] - 5, point2[1] + 2);
+    display.cp437(true);  
+  
+    display.write("-20");
+
+    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
+    // -------------------------------------------------------------------
+    rotatePoint(point, x + 20, y - 10, centerX, centerY, degToRad(thisPin));
+    rotatePoint(point2, i_p - 20, j_p - 10, centerX, centerY, degToRad(thisPin));
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(point2[0] - 5, point2[1] + 2);
+    display.cp437(true);  
+  
+    display.write("20");
+
+    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
+    // -------------------------------------------------------------------
+    rotatePoint(point, x + 20, y - 20, centerX, centerY, degToRad(thisPin));
+    rotatePoint(point2, i_p - 20, j_p - 20, centerX, centerY, degToRad(thisPin));
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(point2[0] - 5, point2[1] + 2);
+    display.cp437(true);  
+  
+    display.write("40");
+
+    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
+    // -------------------------------------------------------------------
+    rotatePoint(point, x + 20, y + 20, centerX, centerY, degToRad(thisPin));
+    rotatePoint(point2, i_p - 20, j_p + 20, centerX, centerY, degToRad(thisPin));
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(point2[0] - 5, point2[1] + 2);
+    display.cp437(true);  
+  
+    display.write("-40");
+
+    display.drawLine(point[0], point[1], point2[0], point2[1], SSD1306_WHITE);
+    // -------------------------------------------------------------------
+    display.display();
+
+  
 }
 
 void rotatePoint(float (& point)[2], float x, float y, float center_x, float center_y, float angle)
@@ -189,107 +193,11 @@ void rotatePoint(float (& point)[2], float x, float y, float center_x, float cen
   point[1] = y_end;
 }
 
-void testdrawrect(void) {
-  display.clearDisplay();
-
-  for(int16_t i=0; i<display.height()/2; i+=2) {
-    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, SSD1306_WHITE);
-    display.display(); // Update screen with each newly-drawn rectangle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillrect(void) {
-  display.clearDisplay();
-
-  for(int16_t i=0; i<display.height()/2; i+=3) {
-    // The INVERSE color is used so rectangles alternate white/black
-    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, SSD1306_INVERSE);
-    display.display(); // Update screen with each newly-drawn rectangle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
 void testdrawcircle(void) {
   display.clearDisplay();
 
   for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
     display.drawCircle(display.width()/2, display.height()/2, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillcircle(void) {
-  display.clearDisplay();
-
-  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=3) {
-    // The INVERSE color is used so circles alternate white/black
-    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_INVERSE);
-    display.display(); // Update screen with each newly-drawn circle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawroundrect(void) {
-  display.clearDisplay();
-
-  for(int16_t i=0; i<display.height()/2-2; i+=2) {
-    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i,
-      display.height()/4, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillroundrect(void) {
-  display.clearDisplay();
-
-  for(int16_t i=0; i<display.height()/2-2; i+=2) {
-    // The INVERSE color is used so round-rects alternate white/black
-    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i,
-      display.height()/4, SSD1306_INVERSE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawtriangle(void) {
-  display.clearDisplay();
-
-  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=5) {
-    display.drawTriangle(
-      display.width()/2  , display.height()/2-i,
-      display.width()/2-i, display.height()/2+i,
-      display.width()/2+i, display.height()/2+i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfilltriangle(void) {
-  display.clearDisplay();
-
-  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=5) {
-    // The INVERSE color is used so triangles alternate white/black
-    display.fillTriangle(
-      display.width()/2  , display.height()/2-i,
-      display.width()/2-i, display.height()/2+i,
-      display.width()/2+i, display.height()/2+i, SSD1306_INVERSE);
     display.display();
     delay(1);
   }
@@ -305,14 +213,18 @@ void testdrawchar(void) {
   display.setCursor(0, 0);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
+  display.setRotation(2);
   display.write("Carlos\n");
-  display.write("Raquel\n");
-  
+  display.display();
+  delay(2000);
+  display.setRotation(0);
+  display.write("Raquel\n");  
   display.display();
   delay(2000);
 
   display.clearDisplay();
   display.setCursor(0, 0);
+  display.setRotation(1);
   display.write("\nCarlos\n");
   display.write("Raquel\n");
   display.display();
